0cbb1e5c1df0458c8c2fbb3df294b524450a8d1e
Alumno <Alumno@DESKTOP-L73PR5B>
2025-02-19 08:45:46 +0100

    Estructura Organizativa del proyecto: Clases Instanciables y Estáticas.

---
+++ PruebaCV_FranDM/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV_FranDM
+{
+    /// <summary>
+    /// Clase estática que sirve de intermediaria entre el Modelo y las Vists de la App
+    /// </summary>
+    public static class Controlador
+    {
+    }
+}
---
+++ PruebaCV_FranDM/InterfazUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV_FranDM
+{
+    //Las clases static son herramientas, trabajan con datos, no mantienen en su estructura datos
+    /// <summary>
+    /// Clase Estática que posibilita los métodos que posibilitan la interacción con el usuario
+    /// </summary>
+    public static class InterfazUsuario
+    {
+
+    }
+}
