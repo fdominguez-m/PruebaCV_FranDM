c153394e09db0de4eeff4ca2f80daee534aa24ce
Alumno <Alumno@DESKTOP-L73PR5B>
2025-02-19 09:07:45 +0100

    Establecimiento de la Estructura del Modelo de Datos (Clase Instanciable)

--- PruebaCV_FranDM/Vehiculo.cs
+++ PruebaCV_FranDM/Vehiculo.cs
@@ -11,5 +11,35 @@ namespace PruebaCV_FranDM
     /// </summary>
     public class Vehiculo
     {
+        #region CONSTANTES - PARÁMETROS DE CONFIGURACIÓN DE LA CLASE
+
+        #endregion
+
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+
+        #endregion
+
+        #region CONSTRUCTORES
+        //Un constructor permite la instanciación de los objetos (la instanciación es obtener presencia en memoria)
+        //Una instancia es también (en programación) ¿abrir programas que compiten por memoria?, ¿cada pestaña de navegador que compite por memoria? un proceso, respuesta final.
+
+        #endregion
+
+        // Mecanismo de acceso a la información que mantiene la clase (datos)
+        // Proporciona dos descriptores de acceso (get y set)
+        // Su misión es la de proteger los datosque entran a l clase (set)
+        // o se transfiere fuera de la clase ()
+        #region PROPIEDADES
+
+        #endregion
+
+        #region MÉTODOS PRIVADOS -  INTERNOS DE LA CLASE
+
+        #endregion
+
+        #region MÉTODOS PÚBLICOS -- ACCESIBLES DESDE FUERA DE LA CLASE
+
+        #endregion
+
     }
 }
